8d9d6d0333caf6d1fcd729bd01d20f7385235015
delandapp <alihandronikolas@gmail.com>
2024-01-25 04:39:06 +0700

    Class Object

---
+++ Object-Class.dart
@@ -0,0 +1,6 @@
+/*
+  ! Jadi dart itu semua adalah object,makanya kalau kita ingin membuat object otomatis merupakan turunan dari class Object Dart
+  ! Ada beberapa field dan method bawaan dari class object
+  * Contohnya adalah toString(), hashCode(), runtimeType()
+
+ */
