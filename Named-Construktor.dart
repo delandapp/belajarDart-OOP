47a8e6d265e77b55006cceda9457fcb3427a0183
delandapp <alihandronikolas@gmail.com>
2024-01-24 22:57:08 +0700

    Named Construktor

---
+++ Named-Construktor.dart
@@ -0,0 +1,24 @@
+// jadi ketika kita ingin membuat lebih dari 1 construktur
+// contoh
+class Person {
+  String name = "Guest";
+  String? address;
+  final String country = "Indonesia";
+  
+  // construktur pertama
+  Person(this.name, this.address);
+
+  // names contruktor with name
+  Person.withName(this.name);
+
+  // named contruktor with address
+  Person.withAddress(this.address);  
+}
+
+void main() {
+  Person person1 = Person('Muhammad Deland', 'Mojosongo');
+
+  Person person2 = Person.withName('Yogha Irgi Kurniawan');
+
+  Person person3 = Person.withAddress('Surakarta');
+}
